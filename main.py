a2c4f8b5692e6f524c1115d91ef82c71a2826237
JDasic01 <77239374+JDasic01@users.noreply.github.com>
2022-11-09 18:23:15 +0100

    stl prototipes

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+
+class Pad(pygame.sprite.Sprite):
+	def __init__(self,type):
+		super().__init__()
+		
+		if type == 'snare':
+			snare = pygame.image.load('').convert_alpha()
+			self.frames = [snare]
+			y_pos  = 300
+		elif type == 'tom':
+			tom = pygame.image.load('').convert_alpha()
+			self.frames = [tom]
+			y_pos  = 300
+		elif type == 'bass':
+			bass = pygame.image.load('').convert_alpha()
+			self.frames = [bass]
+			y_pos  = 300
+		else:
+			cymbal = pygame.image.load('').convert_alpha()
+			self.frames = [cymbal]
+			y_pos  = 300
+
+
+pygame.init()
+screen = pygame.display.set_mode((875,760))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+background_surface = pygame.image.load('drum_kit.png').convert()
+background_rect = background_surface.get_rect(center = (437, 380))
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()		
+		else:
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+				game_active = True
+				start_time = int(pygame.time.get_ticks() / 1000)
+
+	screen.blit(background_surface, background_rect)
+
+	pygame.display.update()
+	clock.tick(60)
